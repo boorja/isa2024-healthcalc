8d37a78383b566381495800164058433d97f2b35
boorja <borja6168@gmail.com>
2024-05-14 21:58:25 +0200

    Implementacion PATRON DECORADOR version europea

---
+++ src/main/java/healthcalc/decoEuropea.java
@@ -0,0 +1,10 @@
+package healthcalc;
+
+public class decoEuropea extends decorador {
+    // ya la tenemos implementada
+
+    public decoEuropea(HealthHospital calculadora) {
+        super(calculadora);
+    }
+    
+}
