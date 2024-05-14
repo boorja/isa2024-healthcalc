00d4d4e4def1a83622a30703b55729ac8c86e411
boorja <borja6168@gmail.com>
2024-05-14 18:30:32 +0200

    Prueba Patron Proxy

--- src/main/java/healthcalc/gui/Main.java
+++ src/main/java/healthcalc/gui/Main.java
@@ -4,7 +4,9 @@ import java.awt.EventQueue;
 
 import healthcalc.HealthCalcImpl;
 import healthcalc.HealthHospital;
+import healthcalc.HealthStats;
 import healthcalc.adapter;
+import healthcalc.proxy;
 
 public class Main {
 
@@ -15,10 +17,27 @@ public class Main {
 
 					//================COMPROBAR ADAPTER================
 
-					HealthHospital calculadora = new adapter();
-					System.out.println(calculadora.bmr('m', 25, 1.77f, 76000));
+					HealthHospital adapter = new adapter();
+					System.out.println("PATRON ADAPTER");
+					System.out.println(adapter.bmr('m', 25, 1.77f, 76000));
 
 					//=================================================
+
+					//================COMPROBAR PROXY================
+					// Inicio
+					proxy proxy = new proxy();
+					proxy.bmr('m', 52, 1.83f, 67675);
+					proxy.bmr('w', 29, 1.64f, 88455);
+					
+					// Comprobacion
+					System.out.println("\n\nPATRON PROXY");
+					System.out.println("Altura media: " + proxy.alturaMedia());
+					System.out.println("Peso medio: " + proxy.pesoMedio());
+					System.out.println("Edad media: " + proxy.edadMedia());
+					System.out.println("BMR medio: " + proxy.bmrMedio());
+					System.out.println("Numero Mujeres: " + proxy.numSexoM());
+					System.out.println("Numero Hombres: " + proxy.numSexoH());
+					System.out.println("Numero Total: " + proxy.numTotalPacientes());
 
 					
 					// HealthCalcImpl healthCalc = HealthCalcImpl.getInstance();
