097828185ed05d2ad425ed83229374657fdd969d
boorja <borja6168@gmail.com>
2024-05-14 21:57:49 +0200

    Implementacion PATRON DECORADOR base

---
+++ src/main/java/healthcalc/decorador.java
@@ -0,0 +1,19 @@
+package healthcalc;
+
+public class decorador implements HealthHospital {
+    protected HealthHospital calculadora;
+
+    public decorador( HealthHospital calculadora){
+        this.calculadora=calculadora;
+    }
+
+    @Override
+    public int pesoIdeal(char genero, float altura) throws Exception {
+        return calculadora.pesoIdeal(genero, altura);
+    }
+    
+    @Override
+    public double bmr(char genero, int edad, float altura, int peso) throws Exception {
+        return calculadora.bmr(genero, edad, altura, peso);
+    }   
+}
