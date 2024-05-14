f8d3e5657d102d6e4df4d5c151f18ead71a7bc0e
boorja <borja6168@gmail.com>
2024-05-14 18:28:59 +0200

    Implementacion Interfaz HealthStats

---
+++ src/main/java/healthcalc/HealthStats.java
@@ -0,0 +1,13 @@
+package healthcalc;
+
+public interface HealthStats {
+
+    public float alturaMedia();
+    public float pesoMedio();
+    public float edadMedia();
+    public float bmrMedio();
+    public int numSexoH();
+    public int numSexoM();
+    public int numTotalPacientes();
+
+}
