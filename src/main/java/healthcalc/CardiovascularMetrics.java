ec479684070082a26e202c34e2ff26ec2757ea9a
boorja <borja6168@gmail.com>
2024-05-28 22:11:29 +0200

    Creacion nuevas interfaces

---
+++ src/main/java/healthcalc/CardiovascularMetrics.java
@@ -0,0 +1,7 @@
+package healthcalc;
+
+public interface CardiovascularMetrics {
+    
+    public double getIdealWeight(int height, char gender) throws Exception;
+    
+}
---
+++ src/main/java/healthcalc/MetabolicMetrics.java
@@ -0,0 +1,5 @@
+package healthcalc;
+
+public interface MetabolicMetrics {
+    public float basalMetabolicRate(float weight, int height, char gender, int age) throws Exception;
+}
